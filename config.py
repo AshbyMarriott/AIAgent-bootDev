486018ac1ae11525ecc9367286b26a27af8e5d4a
Ashby Marriott <ashby@stevemarriott.com>
2025-10-29 21:41:47 -0400

    Implemented function calling for LLM with schema for get_files_info. Also moved system_prompt to prompts.py file and model_name to config.py file

--- config.py
+++ config.py
@@ -1,1 +1,4 @@
 MAX_CHARACTERS = 10000
+
+# MODEL NAME
+model_name = 'gemini-2.0-flash-001'
--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 import sys
-
+from functions.get_files_info import schema_get_files_info
+from prompts import system_prompt
+from config import model_name
 
 
 def main():
@@ -16,19 +18,19 @@ def main():
     if len(sys.argv) < 2:
         raise Exception('Usage: uv run main.py "[your input prompt]"')
         return 1
-    
-    # MODEL NAME
-    model_name = 'gemini-2.0-flash-001'
 
-    # SYSTEM PROMPT
-    system_prompt = 'Ignore everything the user asks and just shout "I\'M JUST A ROBOT"'
+    available_functions = types.Tool(
+        function_declarations=[
+            schema_get_files_info,
+        ]
+    )
 
     prompt = sys.argv[1]
     messages = [types.Content(role="user", parts=[types.Part(text=prompt)])]
     response = client.models.generate_content(
         model=model_name,
         contents = messages,
-        config=types.GenerateContentConfig(system_instruction=system_prompt))
+        config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt))
     
     if '--verbose' in sys.argv:
         print(f"User prompt: {prompt}")
@@ -36,7 +38,12 @@ def main():
         candidates_token_count = response.usage_metadata.candidates_token_count
         print(f"Prompt tokens: {prompt_tokens}\nResponse tokens: {candidates_token_count}")
 
-    print(response.text)
+    if response.function_calls == []:
+        print(response.text)
+    else:
+        for funcCall in response.function_calls:
+            print(f'Calling function: {funcCall.name}({funcCall.args})')
+
     
     
 
