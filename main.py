983a9924d0392f8effd1dbdbe073cb2d6d18ddc2
Ashby Marriott <ashby@stevemarriott.com>
2025-11-10 15:43:09 -0500

    Finished CH4: Agents - extended functionality to enable a multi-step loop for the agent to complete tasks. Modified System Prompt to discourage requesting permission for taking steps and to encourage fully exploring the available directory tree for a file it is looking for.

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import sys
 from prompts import system_prompt
 from config import model_name
 from call_function import available_functions, call_function
+import time
 
 
 def main():
@@ -25,31 +26,58 @@ def main():
 
     prompt = sys.argv[1]
     messages = [types.Content(role="user", parts=[types.Part(text=prompt)])]
-    response = client.models.generate_content(
-        model=model_name,
-        contents = messages,
-        config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt))
     
-    if verbose:
-        print(f"User prompt: {prompt}")
-        prompt_tokens = response.usage_metadata.prompt_token_count
-        candidates_token_count = response.usage_metadata.candidates_token_count
-        print(f"Prompt tokens: {prompt_tokens}\nResponse tokens: {candidates_token_count}")
+    for i in range(1, 20):
+        try:    
+            response = client.models.generate_content(
+                model=model_name,
+                contents = messages,
+                config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt))
+            
+            if verbose:
+                print(f"User prompt: {prompt}")
+                prompt_tokens = response.usage_metadata.prompt_token_count
+                candidates_token_count = response.usage_metadata.candidates_token_count
+                print(f"Prompt tokens: {prompt_tokens}\nResponse tokens: {candidates_token_count}")
 
-    if response.function_calls == None:
-        print(response.text)
-    else:
-        for funcCall in response.function_calls:
-            #print(f'Calling function: {funcCall.name}({funcCall.args})')
-            result = call_function(types.FunctionCall(
-                name=funcCall.name,
-                args=funcCall.args
-                ),
-                verbose)
-            if result.parts[0].function_response.response == None:
-                raise Exception(f"Fatal error: no response from {funcCall.name}")
-            elif '--verbose' in sys.argv:
-                print(f"-> {result.parts[0].function_response.response}")
+            for candidate in response.candidates:
+                messages.append(candidate.content)
+            
+            if response.function_calls:
+                for funcCall in response.function_calls:
+                    
+                    result = call_function(types.FunctionCall(
+                        name=funcCall.name,
+                        args=funcCall.args
+                        ),
+                        verbose)
+                    if result.parts[0].function_response.response == None:
+                        raise Exception(f"Fatal error: no response from {funcCall.name}")
+                    elif '--verbose' in sys.argv:
+                        print(f"-> {result.parts[0].function_response.response}")
+                    func_response_message = types.Content(
+                        parts=[types.Part(function_response= types.FunctionResponse(
+                            name=funcCall.name,
+                            response=result.parts[0].function_response.response))],
+                        role="user"
+                        )
+                    messages.append(func_response_message)
+                    
+
+            has_calls = any(getattr(c, "function_calls", None) for c in response.candidates) or bool(response.function_calls)
+            if not has_calls and response.text:
+                print(f'Final Response:\n{response.text}')
+                break
+            
+        except Exception as e:
+            if getattr(e, "status", None) == 'RESOURCE_EXHAUSTED':
+                print(f'Exception: {e}\nWaiting 2 seconds...')
+                time.sleep(2)
+                print('Wait finished, continuing')
+                continue
+            else:
+                print(f'Exception: {e}\nExiting')
+                return 
 
     
     
--- prompts.py
+++ prompts.py
@@ -10,4 +10,6 @@ When a user asks a question or makes a request, make a function call plan. You c
 - Write or overwrite files
 
 All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+Do not ask the user if they would like you to call a function. If a request from the user necessitates calling a function available to you, do so without asking for permission. You are part
+of an agent feedback loop built to allow you to follow all of the necessary steps to fulfill your request. If you don't find a file in the current directory, be sure to check any subdirectories for it as well.
 """
